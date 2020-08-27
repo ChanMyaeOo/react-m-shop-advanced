51eeabba146c94a7b484b45cc53a4cd3b89b2963
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-08-27 12:15:09 +0630

    added reselect library and implemented for selectCartItemCount

--- src/components/shopping-icon/shopping-icon.component.jsx
+++ src/components/shopping-icon/shopping-icon.component.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import { ReactComponent as ShopIcon } from "../../assets/images/shopping-bag.svg";
 import { toggleCartDropdown } from "../../redux/cart/cart.actions";
+import { selectCartItemCount } from "../../redux/cart/cart.selectors";
 import "./shopping-icon.styles.scss";
 
-const ShoppingIcon = ({ toggleCartDropdown }) => (
+const ShoppingIcon = ({ toggleCartDropdown, itemCount }) => (
   <div className="cart-icon" onClick={() => toggleCartDropdown()}>
     <ShopIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
-export default connect(null, { toggleCartDropdown })(ShoppingIcon);
+
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemCount,
+});
+export default connect(mapStateToProps, { toggleCartDropdown })(ShoppingIcon);
