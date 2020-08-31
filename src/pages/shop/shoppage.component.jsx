f76e4f1e86d9eff0438196690ad0940c15b1cbe1
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-08-31 10:38:07 +0630

    created collection page

--- src/pages/shop/shoppage.component.jsx
+++ src/pages/shop/shoppage.component.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import CollectionOverview from "../../components/collection-overview/collection-overview.component";
+import CollectionPage from "../collection/collection-page.component";
 
-const ShopPage = () => {
-  return <CollectionOverview />;
+const ShopPage = ({ match }) => {
+  return (
+    <div>
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route
+        exact
+        path={`${match.path}/:collectionId`}
+        component={CollectionPage}
+      />
+    </div>
+  );
 };
 
 export default ShopPage;
