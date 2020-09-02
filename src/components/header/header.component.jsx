74ed199cabbea9ea5a4723dbe08d194b12a77387
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-09-03 01:48:07 +0630

    added video background and change poster image at header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,13 +8,13 @@ const Header = () => {
     <div className="outer-container">
       <div className="video-container">
         <video autoPlay loop muted poster={ProductImage}>
-          <source src={""} type="video/mp4" />
+          <source src={Product} type="video/mp4" />
         </video>
       </div>
       <div className="overlay"></div>
       <div className="text">
         <h1 className="header_title">
-          Be your own king of beautiful.
+          Be your own kind of beautiful.
           <br /> Choose what you want in our store?
         </h1>
         <a href="#directory" className="header_explore">
