43f20ebc25dfa0cbef17e69657020a4dc6e29c66
chanmyaeoo <chanmyaeoo1996mgk@gmail.com>
2020-09-04 10:47:28 +0630

    removed hompage.styles.scss and replaced with styled components

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "../../components/header/header.component";
 import Directory from "../../components/directory/directory.component";
+import { HomePageContainer } from "./HomePage.styles";
 
 const HomePage = () => {
   return (
-    <>
+    <HomePageContainer>
       <Header />
       <Directory />
-    </>
+    </HomePageContainer>
   );
 };
 export default HomePage;
